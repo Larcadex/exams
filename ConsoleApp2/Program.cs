59c8d4226d1859fb2d42086da81a32f685923a60
серожа <cepbora@vk.com>
2023-10-18 14:18:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        double[,] data = {
+            { 40, 5 },
+            { 100, 50 },
+            { 60, 200 }
+        };
+
+        bool a = true;
+
+        while (a)
+        {
+            Console.WriteLine("Выберите транспортное средство: \n1 - мопед \n2 - машина \n3 - автобус");
+            int avto_type = 0;
+
+            ConsoleKeyInfo keyInfo1 = Console.ReadKey(true);
+
+            switch (keyInfo1.Key)
+            {
+                case ConsoleKey.D1:
+                    avto_type = 1;
+                    break;
+                case ConsoleKey.D2:
+                    avto_type = 2;
+                    break;
+                case ConsoleKey.D3:
+                    avto_type = 3;
+                    break;
+            }
+            Console.WriteLine();
+
+            double distance = 0;
+
+            Console.WriteLine("Выберите маршрут: \n1 - 100 км \n2 - 200 км \n3 - 300 км");
+            ConsoleKeyInfo keyInfo2 = Console.ReadKey(true);
+
+            switch (keyInfo2.Key)
+            {
+                case ConsoleKey.D1:
+                    distance = 100;
+                    break;
+                case ConsoleKey.D2:
+                    distance = 200;
+                    break;
+                case ConsoleKey.D3:
+                    distance = 300;
+                    break;
+            }
+            Console.WriteLine();
+
+            double speed = data[avto_type - 1, 0]; 
+            double bak_size = data[avto_type - 1, 1];  
+
+            double fuel_per_km = 0;
+
+            switch (avto_type)
+            {
+                case 1:
+                    fuel_per_km = 0.03;
+                    break;
+                case 2:
+                    fuel_per_km = 0.08;
+                    break;
+                case 3:
+                    fuel_per_km = 0.2;
+                    break;
+            }
+
+            double fuel_required = calculate_fuel_required(distance, speed, fuel_per_km);
+            double max_distance = calculate_max_distance(bak_size, fuel_per_km);
+
+            Console.WriteLine($"Для преодоления маршрута в {distance} км на {get_name(avto_type)} со скоростью {speed} км/ч потребуется {fuel_required:F2} литров топлива.");
+
+            if (distance <= max_distance)
+            {
+                Console.WriteLine($"Транспортное средство сможет преодолеть весь маршрут на одном баке.");
+            }
+            else
+            {
+                Console.WriteLine($"Транспортное средство сможет преодолеть максимальное расстояние в {max_distance:F2} км на одном баке.");
+            }
+
+            Console.WriteLine("\nЖелаете запустить программу заново?: \n1 - да \n2 - нет");
+            ConsoleKeyInfo keyInfo3 = Console.ReadKey(true);
+
+            if (keyInfo3.Key == ConsoleKey.D2)
+            {
+                a = false;
+            }
+            Console.Clear();
+        }
+    }
+
+    static double calculate_fuel_required(double distance, double speed, double fuel_per_km)
+    {
+        double fuel_required = (distance / 100) * fuel_per_km * speed;
+        return fuel_required;
+    }
+
+    static double calculate_max_distance(double bak_size, double fuel_per_km)
+    {
+        double max_distance = bak_size / fuel_per_km;
+        return max_distance;
+    }
+
+    static string get_name(int avto_type)
+    {
+        switch (avto_type)
+        {
+            case 1:
+                return "мопеде";
+            case 2:
+                return "машине";
+            case 3:
+                return "автобусе";
+            default:
+                return "неизвестный тип";
+        }
+    }
+}
